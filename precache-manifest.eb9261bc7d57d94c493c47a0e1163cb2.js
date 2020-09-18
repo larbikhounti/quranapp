da5b62f61b7d26cba004f809a8ef1fe2a74353a8
larbikhounti <larbikhounti@gmail.com>
2020-09-18 14:38:34 +0100

    Updates

---
+++ precache-manifest.eb9261bc7d57d94c493c47a0e1163cb2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bd3bc09e4924fde68d0b49dee60ac4a",
+    "url": "/quranapp/index.html"
+  },
+  {
+    "revision": "4a28125acab5cc004113",
+    "url": "/quranapp/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "8c1ca75edf08ff900bd9",
+    "url": "/quranapp/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4a28125acab5cc004113",
+    "url": "/quranapp/static/js/2.2a17b07a.chunk.js"
+  },
+  {
+    "revision": "90377550dd86ed303dd30d8f487872de",
+    "url": "/quranapp/static/js/2.2a17b07a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c1ca75edf08ff900bd9",
+    "url": "/quranapp/static/js/main.7151f580.chunk.js"
+  },
+  {
+    "revision": "7e94f2ea7ee5a82becec",
+    "url": "/quranapp/static/js/runtime-main.a0d422bd.js"
+  }
+]);
